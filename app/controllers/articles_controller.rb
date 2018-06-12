622555c0b6b6931383186b6bbc16c594276b44bc
Sam Rapaport <me@samrapdev.com>
2018-06-12 12:11:34 -0700

    Add update functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
-  def show
+  def edit
     @article = Article.find(params[:id])
   end
 
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update article_params
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
