b2b951b21c765aec32c5badb1f71766d2e26eeb1
Sam Rapaport <me@samrapdev.com>
2018-06-12 20:00:00 -0700

    Add authentication.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with(
+    name: @@auth[:name],
+    password: @@auth[:password],
+    except: [:index, :show]
+  )
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with(
+    name: @@auth[:name],
+    password: @@auth[:password],
+    only: :destroy
+  )
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
